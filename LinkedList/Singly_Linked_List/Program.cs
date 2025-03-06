bb286b97eb4a78fc10aa1c7a6ea799f5df28113b
NihatAliyev <nihataliyev.contact@gmail.com>
2025-03-06 21:26:01 +0400

    Added AddLast method to the LinkedList implementation 🚀

---
+++ LinkedList/Singly_Linked_List/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace DataStructures_Singly_LinkedList
+{
+    class Program
+    {
+        static void Main()
+        {
+            Singly_LinkedList list = new Singly_LinkedList();
+            list.Addlast(12);
+            list.Addlast(15);
+            list.Addlast(18);
+            list.ShowInfo();
+        }
+    }
+    class Node
+    {
+        public int Data {get; }
+        public Node? Next {get; private set;}
+        public Node(int data)
+        {
+            this.Data = data;
+            this.Next = null;
+        }
+
+        public void SetNext(Node NextNode)
+        {
+            this.Next = NextNode;
+        }
+    }
+    class Singly_LinkedList
+    {
+        private Node? Head;
+
+        public Singly_LinkedList()
+        {
+            this.Head = null;
+        }
+
+        public void Addlast(int data)
+        {
+            Node current = new Node(data);
+            if(Head == null)
+            {
+                Head = current;
+            }
+            else
+            {
+                Node temp = Head;
+                while(temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.SetNext(current);
+            }
+        }
+        public void ShowInfo()
+        {
+            Node? temp = Head;
+            while(temp!=null)
+            {
+                System.Console.Write($"{temp.Data} --->> ");
+                temp = temp.Next;
+            }
+            System.Console.Write("NULL");
+        }    
+    }
+    
+}

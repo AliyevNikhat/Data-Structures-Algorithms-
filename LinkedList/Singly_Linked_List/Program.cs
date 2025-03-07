b5f052b5dc4cf1ecf4f2f6b197e59564502fbbad
NihatAliyev <nihataliyev.contact@gmail.com>
2025-03-07 21:53:07 +0400

    AddMiddle and RemoveFirst method to the LinkedList implementation 🚀

--- LinkedList/Singly_Linked_List/Program.cs
+++ LinkedList/Singly_Linked_List/Program.cs
@@ -11,22 +11,21 @@ namespace DataStructures_Singly_LinkedList
             list.Addlast(15);
             list.Addlast(18);
             list.AddFirst(9);
+            list.AddMiddle(16);
+            list.ShowInfo();
+            list.RemoveFirst();
+            Console.WriteLine();
             list.ShowInfo();
         }
     }
     class Node
     {
         public int Data {get; }
-        public Node? Next {get; private set;}
+        public Node? Next {get; set;}
         public Node(int data)
         {
             this.Data = data;
             this.Next = null;
-        }
-
-        public void SetNext(Node NextNode)
-        {
-            this.Next = NextNode;
         }
     }
     class Singly_LinkedList
@@ -52,7 +51,7 @@ namespace DataStructures_Singly_LinkedList
                 {
                     temp = temp.Next;
                 }
-                temp.SetNext(current);
+                temp.Next = current;
             }
         }
         public void AddFirst(int data)
@@ -64,7 +63,7 @@ namespace DataStructures_Singly_LinkedList
             }
             else
             {
-                current.SetNext(Head);
+                current.Next = Head;
                 Head = current;
             }
         }
@@ -78,7 +77,48 @@ namespace DataStructures_Singly_LinkedList
             }
             System.Console.Write("NULL");
         }    
-
+        public void AddMiddle(int data)
+        {
+            Node current = new Node(data);
+            if(Head == null)
+            {
+                Head = current;
+            }
+            else
+            {
+                int middle = GetLinkedListCount() / 2;
+                Node temp = Head;
+                while(middle > 1)
+                {
+                    temp = temp.Next;
+                    middle--;
+                }
+                current.Next = temp.Next;
+                temp.Next = current;
+            }
+        }
+        public int GetLinkedListCount()
+        {
+            Node temp = Head;
+            int count = 0;
+            while(temp != null)
+            {
+                count++;
+                temp = temp.Next;
+            }
+            return count;
+        }
+        public void RemoveFirst()
+        {
+            if(Head == null)
+            {
+                throw new Exception("There is not Element in Singly LinkedList !!");
+            }
+            else
+            {
+                Head = Head.Next;
+            }
+        }
     }
     
 }

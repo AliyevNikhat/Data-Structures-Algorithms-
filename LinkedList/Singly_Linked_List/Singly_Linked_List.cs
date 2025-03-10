e6308ec1749270a2d34f0cbabf1a69e7270f8d6a
NihatAliyev <nihataliyev.contact@gmail.com>
2025-03-10 22:06:16 +0400

    AddLast and ShowInfo method to the LinkedList implementation!

---
+++ LinkedList/Singly_Linked_List/Singly_Linked_List.cs
@@ -0,0 +1,69 @@
+using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main()
+        {
+            Singly_Linked_List list = new Singly_Linked_List();
+            list.AddLast(12);
+            list.AddLast(15);
+            list.AddLast(18);
+            list.AddLast(21);
+            list.ShowInfo();
+        }
+    }
+    class Node
+    {
+        public int Data { get; }
+        public Node? Next {get; set;}
+        public Node(int data)
+        {
+            this.Data = data;
+            this.Next = null;
+        }
+    }
+    class Singly_Linked_List
+    {
+        Node? Head;
+        public Singly_Linked_List()
+        {
+            this.Head = null;
+        }
+        public void AddLast(int data)
+        {
+            Node current = new Node(data);
+            if(Head == null)
+            {
+                Head = current;
+            }
+            else
+            {
+                Node temp = Head;
+                while(temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = current;
+            }
+        }
+        public void ShowInfo()
+        {
+            if(Head == null)
+            {
+                System.Console.WriteLine("There is not Element in Singly_LinedList!");
+            }
+            else
+            {
+                Node temp = Head;
+                while(temp != null)
+                {
+                    System.Console.Write($"{temp.Data} --->>> ");
+                    temp = temp.Next;
+                }
+                System.Console.Write(" NULL");
+            }
+        }
+    }
+}
